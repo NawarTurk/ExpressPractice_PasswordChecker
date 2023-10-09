c4e9e2605d262fc771988f00aa53d1eabeb7d390
Nawar T <nawarturk11@gmail.com>
2023-10-08 23:18:41 -0400

    Implementing index.js

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
-import  express from "express";
+import  express, { response } from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3001;
 const __dirname = dirname(fileURLToPath(import.meta.url));
+const password = "ILoveCoding!"
+let isCorrectPassword = false;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function passwordChecker(req, res, next) {
+    if (req.body.password === password) isCorrectPassword = true;
+    next();
+}
+
+app.use(passwordChecker)
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html")
 })
 
 app.post("/check", (req, res) => {
-    res.send("test")
+    if (isCorrectPassword) {
+        res.sendFile(__dirname + '/public/coding-tips.html')
+        isCorrectPassword = false;
+    } else {
+        res.redirect("/");
+    }
 });
 
 app.listen(port, () => {
